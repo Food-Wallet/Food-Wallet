6deee8d96b97528fd94fc7d01423d85f53d0b673
Chaos0103 <lyt1228@naver.com>
2024-01-17 05:00:20 +0900

    test: 메뉴 정보 수정 REST Docs 테스트 코드 구현

--- server/src/test/java/com/foodwallet/server/docs/menu/MenuApiControllerDocsTest.java
+++ server/src/test/java/com/foodwallet/server/docs/menu/MenuApiControllerDocsTest.java
@@ -1,6 +1,8 @@
 package com.foodwallet.server.docs.menu;
 
 import com.foodwallet.server.api.controller.menu.MenuApiController;
+import com.foodwallet.server.api.controller.menu.request.MenuModifyRequest;
+import com.foodwallet.server.api.controller.store.request.StoreModifyRequest;
 import com.foodwallet.server.docs.RestDocsSupport;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -14,10 +16,10 @@ import static org.springframework.restdocs.headers.HeaderDocumentation.headerWit
 import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.multipart;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.patch;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
-import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
-import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.restdocs.request.RequestDocumentation.*;
 import static org.springframework.restdocs.request.RequestDocumentation.partWithName;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -94,6 +96,67 @@ public class MenuApiControllerDocsTest extends RestDocsSupport {
                         .description("등록된 메뉴 가격"),
                     fieldWithPath("data.createdDateTime").type(JsonFieldType.ARRAY)
                         .description("메뉴 등록 일시")
+                )
+            ));
+    }
+
+    @DisplayName("메뉴 정보 수정 API")
+    @Test
+    void modifyMenuInfo() throws Exception {
+        MenuModifyRequest request = MenuModifyRequest.builder()
+            .name("간장닭강정")
+            .description("우리 매장 시그니처 메뉴입니다!")
+            .price(8000)
+            .build();
+
+        mockMvc.perform(
+                patch(BASE_URL + "/{menuId}", 1, 1)
+                    .header(HttpHeaders.AUTHORIZATION, "Bearer jwt.access.token")
+                    .content(objectMapper.writeValueAsString(request))
+                    .contentType(MediaType.APPLICATION_JSON)
+            )
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andDo(document("modify-menu-info",
+                preprocessRequest(prettyPrint()),
+                preprocessResponse(prettyPrint()),
+                requestHeaders(
+                    headerWithName(HttpHeaders.AUTHORIZATION)
+                        .description("JWT 접근 토큰")
+                ),
+                pathParameters(
+                    parameterWithName("storeId")
+                        .description("매장 식별키"),
+                    parameterWithName("menuId")
+                        .description("메뉴 식별키")
+                ),
+                requestFields(
+                    fieldWithPath("name").type(JsonFieldType.STRING)
+                        .description("메뉴 이름"),
+                    fieldWithPath("description").type(JsonFieldType.STRING)
+                        .optional()
+                        .description("메뉴 설명"),
+                    fieldWithPath("price").type(JsonFieldType.NUMBER)
+                        .description("메뉴 가격")
+                ),
+                responseFields(
+                    fieldWithPath("code").type(JsonFieldType.NUMBER)
+                        .description("코드"),
+                    fieldWithPath("status").type(JsonFieldType.STRING)
+                        .description("상태"),
+                    fieldWithPath("message").type(JsonFieldType.STRING)
+                        .description("메시지"),
+                    fieldWithPath("data").type(JsonFieldType.OBJECT)
+                        .description("응답 데이터"),
+                    fieldWithPath("data.name").type(JsonFieldType.STRING)
+                        .description("수정된 메뉴 이름"),
+                    fieldWithPath("data.description").type(JsonFieldType.STRING)
+                        .optional()
+                        .description("수정된 메뉴 설명"),
+                    fieldWithPath("data.price").type(JsonFieldType.NUMBER)
+                        .description("수정된 메뉴 가격"),
+                    fieldWithPath("data.modifiedDateTime").type(JsonFieldType.ARRAY)
+                        .description("메뉴 정보 수정 일시")
                 )
             ));
     }

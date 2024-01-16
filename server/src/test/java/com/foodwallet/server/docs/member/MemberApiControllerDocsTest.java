5f5fddad016281c0868a70d119a5a71588daa94d
Chaos0103 <lyt1228@naver.com>
2024-01-16 15:34:27 +0900

    test: 비밀번호 변경 REST Docs 테스트 코드 구현

---
+++ server/src/test/java/com/foodwallet/server/docs/member/MemberApiControllerDocsTest.java
@@ -0,0 +1,74 @@
+package com.foodwallet.server.docs.member;
+
+import com.foodwallet.server.api.controller.member.MemberApiController;
+import com.foodwallet.server.api.controller.member.request.CheckEmailDuplicationRequest;
+import com.foodwallet.server.api.controller.member.request.PwdModifyRequest;
+import com.foodwallet.server.docs.RestDocsSupport;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.restdocs.payload.JsonFieldType;
+
+import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
+import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class MemberApiControllerDocsTest extends RestDocsSupport {
+
+    private static final String BASE_URL = "/api/v1/members";
+
+    @Override
+    protected Object initController() {
+        return new MemberApiController();
+    }
+
+    @DisplayName("비밀번호 변경 API")
+    @Test
+    void modifyPwd() throws Exception {
+        PwdModifyRequest request = PwdModifyRequest.builder()
+            .currentPwd("dong1234!")
+            .newPwd("dong5678@")
+            .build();
+
+        mockMvc.perform(
+                patch(BASE_URL + "/pwd")
+                    .header(HttpHeaders.AUTHORIZATION, "Bearer jwt.access.token")
+                    .content(objectMapper.writeValueAsString(request))
+                    .contentType(MediaType.APPLICATION_JSON)
+            )
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andDo(document("modify-pwd",
+                preprocessRequest(prettyPrint()),
+                preprocessResponse(prettyPrint()),
+                requestHeaders(
+                    headerWithName(HttpHeaders.AUTHORIZATION)
+                        .description("JWT 접근 토큰")
+                ),
+                requestFields(
+                    fieldWithPath("currentPwd").type(JsonFieldType.STRING)
+                        .description("현재 비밀번호"),
+                    fieldWithPath("newPwd").type(JsonFieldType.STRING)
+                        .description("새로운 비밀번호")
+                ),
+                responseFields(
+                    fieldWithPath("code").type(JsonFieldType.NUMBER)
+                        .description("코드"),
+                    fieldWithPath("status").type(JsonFieldType.STRING)
+                        .description("상태"),
+                    fieldWithPath("message").type(JsonFieldType.STRING)
+                        .description("메시지"),
+                    fieldWithPath("data").type(JsonFieldType.NULL)
+                        .description("응답 데이터")
+                )
+            ));
+    }
+}

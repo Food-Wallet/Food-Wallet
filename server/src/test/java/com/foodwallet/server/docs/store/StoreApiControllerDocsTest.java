a1e88bb8e4a9fc70d3f08a6f485d3805c64eb508
Chaos0103 <lyt1228@naver.com>
2024-01-16 17:58:11 +0900

    test: 매장 오픈 REST Docs 테스트 코드 구현

--- server/src/test/java/com/foodwallet/server/docs/store/StoreApiControllerDocsTest.java
+++ server/src/test/java/com/foodwallet/server/docs/store/StoreApiControllerDocsTest.java
@@ -2,6 +2,7 @@ package com.foodwallet.server.docs.store;
 
 import com.foodwallet.server.api.controller.store.StoreApiController;
 import com.foodwallet.server.api.controller.store.request.StoreCreateRequest;
+import com.foodwallet.server.api.controller.store.request.StoreOpenRequest;
 import com.foodwallet.server.docs.RestDocsSupport;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -12,9 +13,12 @@ import org.springframework.restdocs.payload.JsonFieldType;
 import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
 import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.patch;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -77,7 +81,66 @@ public class StoreApiControllerDocsTest extends RestDocsSupport {
                         .optional()
                         .description("매장 설명"),
                     fieldWithPath("data.createdDateTime").type(JsonFieldType.ARRAY)
-                        .description("매장 설명")
+                        .description("매장 등록 일시")
+                )
+            ));
+    }
+
+    @DisplayName("매장 오픈 API")
+    @Test
+    void openStore() throws Exception {
+        StoreOpenRequest request = StoreOpenRequest.builder()
+            .address("서울 중구 세종대로 110")
+            .latitude(37.566352778)
+            .longitude(126.977952778)
+            .build();
+
+        mockMvc.perform(
+                patch(BASE_URL + "/{storeId}", 1)
+                    .header(HttpHeaders.AUTHORIZATION, "Bearer jwt.access.token")
+                    .content(objectMapper.writeValueAsString(request))
+                    .contentType(MediaType.APPLICATION_JSON)
+            )
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andDo(document("open-store",
+                preprocessRequest(prettyPrint()),
+                preprocessResponse(prettyPrint()),
+                requestHeaders(
+                    headerWithName(HttpHeaders.AUTHORIZATION)
+                        .description("JWT 접근 토큰")
+                ),
+                pathParameters(
+                    parameterWithName("storeId")
+                        .description("매장 식별키")
+                ),
+                requestFields(
+                    fieldWithPath("address").type(JsonFieldType.STRING)
+                        .description("현재 위치 도로명 주소"),
+                    fieldWithPath("latitude").type(JsonFieldType.NUMBER)
+                        .description("현재 위치 위도"),
+                    fieldWithPath("longitude").type(JsonFieldType.NUMBER)
+                        .description("현재 위치 경도")
+                ),
+                responseFields(
+                    fieldWithPath("code").type(JsonFieldType.NUMBER)
+                        .description("코드"),
+                    fieldWithPath("status").type(JsonFieldType.STRING)
+                        .description("상태"),
+                    fieldWithPath("message").type(JsonFieldType.STRING)
+                        .description("메시지"),
+                    fieldWithPath("data").type(JsonFieldType.OBJECT)
+                        .description("응답 데이터"),
+                    fieldWithPath("data.name").type(JsonFieldType.STRING)
+                        .description("매장 이름"),
+                    fieldWithPath("data.address").type(JsonFieldType.STRING)
+                        .description("매장 운영 주소"),
+                    fieldWithPath("data.latitude").type(JsonFieldType.NUMBER)
+                        .description("매장 운영 위도"),
+                    fieldWithPath("data.longitude").type(JsonFieldType.NUMBER)
+                        .description("매장 운영 경도"),
+                    fieldWithPath("data.openDateTime").type(JsonFieldType.ARRAY)
+                        .description("매장 오픈 시간")
                 )
             ));
     }

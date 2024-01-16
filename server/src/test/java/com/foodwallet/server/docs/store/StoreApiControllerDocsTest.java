9b7b1414ca578cf4b2f82656218dc5c52c14bb28
Chaos0103 <lyt1228@naver.com>
2024-01-17 03:08:44 +0900

    test: 매장 목록 조회 REST Docs 테스트 코드 구현

--- server/src/test/java/com/foodwallet/server/docs/store/StoreApiControllerDocsTest.java
+++ server/src/test/java/com/foodwallet/server/docs/store/StoreApiControllerDocsTest.java
@@ -14,13 +14,11 @@ import org.springframework.restdocs.payload.JsonFieldType;
 import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
 import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
-import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.delete;
-import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.patch;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
-import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
-import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
+import static org.springframework.restdocs.request.RequestDocumentation.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -286,6 +284,73 @@ public class StoreApiControllerDocsTest extends RestDocsSupport {
                         .description("매장명"),
                     fieldWithPath("data.removedDateTime").type(JsonFieldType.ARRAY)
                         .description("매장 삭제 일시")
+                )
+            ));
+    }
+
+    @DisplayName("매장 조회 API")
+    @Test
+    void searchStores() throws Exception {
+        mockMvc.perform(
+                get(BASE_URL)
+                    .header(HttpHeaders.AUTHORIZATION, "Bearer jwt.access.token")
+                    .param("type", "치킨")
+                    .param("query", "닭강정")
+                    .param("page", "1")
+            )
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andDo(document("search-stores",
+                preprocessRequest(prettyPrint()),
+                preprocessResponse(prettyPrint()),
+                requestHeaders(
+                    headerWithName(HttpHeaders.AUTHORIZATION)
+                        .description("JWT 접근 토큰")
+                ),
+                queryParameters(
+                    parameterWithName("type")
+                        .optional()
+                        .description("검색할 매장 타입"),
+                    parameterWithName("query")
+                        .optional()
+                        .description("검색할 매장명"),
+                    parameterWithName("page")
+                        .optional()
+                        .description("페이지 번호")
+                ),
+                responseFields(
+                    fieldWithPath("code").type(JsonFieldType.NUMBER)
+                        .description("코드"),
+                    fieldWithPath("status").type(JsonFieldType.STRING)
+                        .description("상태"),
+                    fieldWithPath("message").type(JsonFieldType.STRING)
+                        .description("메시지"),
+                    fieldWithPath("data").type(JsonFieldType.OBJECT)
+                        .description("응답 데이터"),
+                    fieldWithPath("data.content").type(JsonFieldType.ARRAY)
+                        .description("매장 데이터"),
+                    fieldWithPath("data.content[].storeId").type(JsonFieldType.NUMBER)
+                        .description("매장 식별키"),
+                    fieldWithPath("data.content[].type").type(JsonFieldType.STRING)
+                        .description("매장 타입"),
+                    fieldWithPath("data.content[].name").type(JsonFieldType.STRING)
+                        .description("매장명"),
+                    fieldWithPath("data.content[].address").type(JsonFieldType.STRING)
+                        .description("매장 운영 주소"),
+                    fieldWithPath("data.content[].openTime").type(JsonFieldType.STRING)
+                        .description("매장 운영 시간"),
+                    fieldWithPath("data.content[].storeImage").type(JsonFieldType.STRING)
+                        .description("매장 썸네일 이미지 주소"),
+                    fieldWithPath("data.content[].avgRate").type(JsonFieldType.NUMBER)
+                        .description("매장 리뷰 별점 평균"),
+                    fieldWithPath("data.currentPage").type(JsonFieldType.NUMBER)
+                        .description("현재 페이지 번호"),
+                    fieldWithPath("data.size").type(JsonFieldType.NUMBER)
+                        .description("데이터 요청 갯수"),
+                    fieldWithPath("data.isFirst").type(JsonFieldType.BOOLEAN)
+                        .description("첫 페이지 여부"),
+                    fieldWithPath("data.isLast").type(JsonFieldType.BOOLEAN)
+                        .description("마지막 페이지 여부")
                 )
             ));
     }

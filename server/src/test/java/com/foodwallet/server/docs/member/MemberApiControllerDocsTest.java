c4c3526e65e5d94a17811bb157a5c4478e6bca8c
Chaos0103 <lyt1228@naver.com>
2024-01-16 15:42:05 +0900

    test: 회원 탈퇴 REST Docs 테스트 코드 구현

--- server/src/test/java/com/foodwallet/server/docs/member/MemberApiControllerDocsTest.java
+++ server/src/test/java/com/foodwallet/server/docs/member/MemberApiControllerDocsTest.java
@@ -2,6 +2,7 @@ package com.foodwallet.server.docs.member;
 
 import com.foodwallet.server.api.controller.member.MemberApiController;
 import com.foodwallet.server.api.controller.member.request.CheckEmailDuplicationRequest;
+import com.foodwallet.server.api.controller.member.request.MemberWithdrawalRequest;
 import com.foodwallet.server.api.controller.member.request.PwdModifyRequest;
 import com.foodwallet.server.docs.RestDocsSupport;
 import org.junit.jupiter.api.DisplayName;
@@ -68,6 +69,51 @@ public class MemberApiControllerDocsTest extends RestDocsSupport {
                         .description("메시지"),
                     fieldWithPath("data").type(JsonFieldType.NULL)
                         .description("응답 데이터")
+                )
+            ));
+    }
+
+    @DisplayName("회원 탈퇴 API")
+    @Test
+    void withdrawal() throws Exception {
+        MemberWithdrawalRequest request = MemberWithdrawalRequest.builder()
+            .pwd("dong1234!")
+            .build();
+
+        mockMvc.perform(
+                patch(BASE_URL + "/withdrawal")
+                    .header(HttpHeaders.AUTHORIZATION, "Bearer jwt.access.token")
+                    .content(objectMapper.writeValueAsString(request))
+                    .contentType(MediaType.APPLICATION_JSON)
+            )
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andDo(document("withdrawal",
+                preprocessRequest(prettyPrint()),
+                preprocessResponse(prettyPrint()),
+                requestHeaders(
+                    headerWithName(HttpHeaders.AUTHORIZATION)
+                        .description("JWT 접근 토큰")
+                ),
+                requestFields(
+                    fieldWithPath("pwd").type(JsonFieldType.STRING)
+                        .description("현재 비밀번호")
+                ),
+                responseFields(
+                    fieldWithPath("code").type(JsonFieldType.NUMBER)
+                        .description("코드"),
+                    fieldWithPath("status").type(JsonFieldType.STRING)
+                        .description("상태"),
+                    fieldWithPath("message").type(JsonFieldType.STRING)
+                        .description("메시지"),
+                    fieldWithPath("data").type(JsonFieldType.OBJECT)
+                        .description("응답 데이터"),
+                    fieldWithPath("data.email").type(JsonFieldType.STRING)
+                        .description("탈퇴한 계정 이메일"),
+                    fieldWithPath("data.name").type(JsonFieldType.STRING)
+                        .description("탈퇴한 계정 회원명"),
+                    fieldWithPath("data.withdrawalDateTime").type(JsonFieldType.ARRAY)
+                        .description("탈퇴 일시")
                 )
             ));
     }

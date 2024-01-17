153a4ad6e38987953621ae3a5c66ff52abeceac7
Chaos0103 <lyt1228@naver.com>
2024-01-17 16:10:53 +0900

    test: 매장 즐겨찾기 취소 REST Docs 테스트 코드 구현

--- server/src/test/java/com/foodwallet/server/docs/bookmark/BookmarkApiControllerDocsTest.java
+++ server/src/test/java/com/foodwallet/server/docs/bookmark/BookmarkApiControllerDocsTest.java
@@ -11,6 +11,7 @@ import org.springframework.restdocs.payload.JsonFieldType;
 import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
 import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.delete;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
@@ -61,7 +62,43 @@ public class BookmarkApiControllerDocsTest extends RestDocsSupport {
                     fieldWithPath("data").type(JsonFieldType.OBJECT)
                         .description("응답 데이터"),
                     fieldWithPath("data.storeName").type(JsonFieldType.STRING)
-                        .description("즐겨찾기 한 매장명")
+                        .description("즐겨찾기 등록한 매장명")
+                )
+            ));
+    }
+
+    @DisplayName("매장 즐겨찾기 취소 API")
+    @Test
+    void cancelBookmark() throws Exception {
+        mockMvc.perform(
+                delete(BASE_URL + "/stores/{storeId}/bookmark", 1)
+                    .header(HttpHeaders.AUTHORIZATION, "Bearer jwt.access.token")
+                    .contentType(MediaType.APPLICATION_JSON)
+            )
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andDo(document("cancel-bookmark",
+                preprocessRequest(prettyPrint()),
+                preprocessResponse(prettyPrint()),
+                requestHeaders(
+                    headerWithName(HttpHeaders.AUTHORIZATION)
+                        .description("JWT 접근 토큰")
+                ),
+                pathParameters(
+                    parameterWithName("storeId")
+                        .description("매장 식별키")
+                ),
+                responseFields(
+                    fieldWithPath("code").type(JsonFieldType.NUMBER)
+                        .description("코드"),
+                    fieldWithPath("status").type(JsonFieldType.STRING)
+                        .description("상태"),
+                    fieldWithPath("message").type(JsonFieldType.STRING)
+                        .description("메시지"),
+                    fieldWithPath("data").type(JsonFieldType.OBJECT)
+                        .description("응답 데이터"),
+                    fieldWithPath("data.storeName").type(JsonFieldType.STRING)
+                        .description("즐겨찾기 취소한 매장명")
                 )
             ));
     }

c76ce1b9c330ad5105b55495e24f87aba229a5dc
Chaos0103 <lyt1228@naver.com>
2024-01-17 16:17:04 +0900

    feat: 나의 즐겨찾기 매장 조회 API 스켈레톤 코드 구현

--- server/src/main/java/com/foodwallet/server/api/controller/bookmark/BookmarkApiController.java
+++ server/src/main/java/com/foodwallet/server/api/controller/bookmark/BookmarkApiController.java
@@ -1,10 +1,16 @@
 package com.foodwallet.server.api.controller.bookmark;
 
 import com.foodwallet.server.api.ApiResponse;
+import com.foodwallet.server.api.SliceResponse;
 import com.foodwallet.server.api.service.bookmark.response.BookmarkCancelResponse;
 import com.foodwallet.server.api.service.bookmark.response.BookmarkCreateResponse;
+import com.foodwallet.server.api.service.bookmark.response.BookmarkResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.SliceImpl;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -25,5 +31,26 @@ public class BookmarkApiController {
             .storeName("나리닭강정")
             .build();
         return ApiResponse.ok(response);
+    }
+
+    @GetMapping("/bookmarks")
+    public ApiResponse<SliceResponse<BookmarkResponse>> searchBookmarks(
+        @RequestParam Integer page
+    ) {
+        BookmarkResponse response = BookmarkResponse.builder()
+            .storeId(1L)
+            .type("치킨")
+            .name("나리닭강정")
+            .address("서울 중구 세종대로 110")
+            .openTime("오전 11:00 ~ 오후 8:00")
+            .storeImage("s3-store-img-url")
+            .avgRate(5.0)
+            .build();
+        PageRequest pageable = PageRequest.of(0, 10);
+        SliceImpl<BookmarkResponse> slice = new SliceImpl<>(List.of(response), pageable, false);
+
+        SliceResponse<BookmarkResponse> data = SliceResponse.of(slice);
+
+        return ApiResponse.ok(data);
     }
 }

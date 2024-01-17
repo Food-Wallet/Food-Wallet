aa3c1abe4a6ea379219e883040667bf96c14b076
Chaos0103 <lyt1228@naver.com>
2024-01-17 19:16:01 +0900

    test: 주문 상세 조회 REST Docs 테스트 코드 구현

--- server/src/test/java/com/foodwallet/server/docs/order/OrderApiControllerDocsTest.java
+++ server/src/test/java/com/foodwallet/server/docs/order/OrderApiControllerDocsTest.java
@@ -137,4 +137,59 @@ public class OrderApiControllerDocsTest extends RestDocsSupport {
                 )
             ));
     }
+
+    @DisplayName("주문 상세 조회 API")
+    @Test
+    void searchOrder() throws Exception {
+        mockMvc.perform(
+                get(BASE_URL + "/orders/{orderId}", 1)
+                    .header(HttpHeaders.AUTHORIZATION, "Bearer jwt.access.token")
+                    .contentType(MediaType.APPLICATION_JSON)
+            )
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andDo(document("search-order",
+                preprocessRequest(prettyPrint()),
+                preprocessResponse(prettyPrint()),
+                requestHeaders(
+                    headerWithName(HttpHeaders.AUTHORIZATION)
+                        .description("JWT 접근 토큰")
+                ),
+                pathParameters(
+                    parameterWithName("orderId")
+                        .description("주문 식별키")
+                ),
+                responseFields(
+                    fieldWithPath("code").type(JsonFieldType.NUMBER)
+                        .description("코드"),
+                    fieldWithPath("status").type(JsonFieldType.STRING)
+                        .description("상태"),
+                    fieldWithPath("message").type(JsonFieldType.STRING)
+                        .description("메시지"),
+                    fieldWithPath("data").type(JsonFieldType.OBJECT)
+                        .description("응답 데이터"),
+                    fieldWithPath("data.orderId").type(JsonFieldType.NUMBER)
+                        .description("주문 식별키"),
+                    fieldWithPath("data.storeName").type(JsonFieldType.STRING)
+                        .description("주문한 매장명"),
+                    fieldWithPath("data.orderStatus").type(JsonFieldType.STRING)
+                        .description("주문 상태"),
+                    fieldWithPath("data.totalPrice").type(JsonFieldType.NUMBER)
+                        .description("총 주문 금액"),
+                    fieldWithPath("data.orderDateTime").type(JsonFieldType.ARRAY)
+                        .description("주문 일시"),
+                    fieldWithPath("data.orderMenus").type(JsonFieldType.ARRAY)
+                        .description("주문한 메뉴 목록"),
+                    fieldWithPath("data.orderMenus[].menuName").type(JsonFieldType.STRING)
+                        .description("주문한 메뉴명"),
+                    fieldWithPath("data.orderMenus[].menuImage").type(JsonFieldType.STRING)
+                        .description("주문한 메뉴 이미지"),
+                    fieldWithPath("data.orderMenus[].orderPrice").type(JsonFieldType.NUMBER)
+                        .description("주문 금액"),
+                    fieldWithPath("data.orderMenus[].count").type(JsonFieldType.NUMBER)
+                        .description("주문 수량")
+                )
+            ));
+
+    }
 }

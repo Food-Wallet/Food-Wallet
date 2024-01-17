57060cf92b264951380eeca6654dc8f01f918932
Chaos0103 <lyt1228@naver.com>
2024-01-17 16:06:28 +0900

    test: 매장 즐겨찾기 등록 REST Docs 테스트 코드 구현

---
+++ server/src/test/java/com/foodwallet/server/docs/bookmark/BookmarkApiControllerDocsTest.java
@@ -0,0 +1,68 @@
+package com.foodwallet.server.docs.bookmark;
+
+import com.foodwallet.server.api.controller.bookmark.BookmarkApiController;
+import com.foodwallet.server.docs.RestDocsSupport;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.restdocs.payload.JsonFieldType;
+
+import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
+import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class BookmarkApiControllerDocsTest extends RestDocsSupport {
+
+    private static final String BASE_URL = "/api/v1";
+
+    @Override
+    protected Object initController() {
+        return new BookmarkApiController();
+    }
+
+    @DisplayName("매장 즐겨찾기 등록 API")
+    @Test
+    void createBookmark() throws Exception {
+        mockMvc.perform(
+                post(BASE_URL + "/stores/{storeId}/bookmark", 1)
+                    .header(HttpHeaders.AUTHORIZATION, "Bearer jwt.access.token")
+                    .contentType(MediaType.APPLICATION_JSON)
+            )
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andDo(document("create-bookmark",
+                preprocessRequest(prettyPrint()),
+                preprocessResponse(prettyPrint()),
+                requestHeaders(
+                    headerWithName(HttpHeaders.AUTHORIZATION)
+                        .description("JWT 접근 토큰")
+                ),
+                pathParameters(
+                    parameterWithName("storeId")
+                        .description("매장 식별키")
+                ),
+                responseFields(
+                    fieldWithPath("code").type(JsonFieldType.NUMBER)
+                        .description("코드"),
+                    fieldWithPath("status").type(JsonFieldType.STRING)
+                        .description("상태"),
+                    fieldWithPath("message").type(JsonFieldType.STRING)
+                        .description("메시지"),
+                    fieldWithPath("data").type(JsonFieldType.OBJECT)
+                        .description("응답 데이터"),
+                    fieldWithPath("data.storeName").type(JsonFieldType.STRING)
+                        .description("즐겨찾기 한 매장명")
+                )
+            ));
+    }
+}

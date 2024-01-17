87c107961a1b6421fa2156fbc910d9442c37c0f5
Chaos0103 <lyt1228@naver.com>
2024-01-17 18:42:06 +0900

    feat: 주문 조회 API 스켈레톤 코드 구현

--- server/src/main/java/com/foodwallet/server/api/controller/order/OrderApiController.java
+++ server/src/main/java/com/foodwallet/server/api/controller/order/OrderApiController.java
@@ -1,9 +1,14 @@
 package com.foodwallet.server.api.controller.order;
 
 import com.foodwallet.server.api.ApiResponse;
+import com.foodwallet.server.api.SliceResponse;
 import com.foodwallet.server.api.service.order.response.OrderCreateResponse;
 import com.foodwallet.server.api.service.order.response.OrderMenuResponse;
+import com.foodwallet.server.api.service.order.response.OrderResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.SliceImpl;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
@@ -11,10 +16,11 @@ import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/api/v1/baskets/{basketId}/orders")
+@RequestMapping("/api/v1")
 public class OrderApiController {
 
-    @PostMapping
+    @PostMapping("/baskets/{basketId}/orders")
+    @ResponseStatus(HttpStatus.CREATED)
     public ApiResponse<OrderCreateResponse> createOrder(@PathVariable Long basketId) {
         OrderMenuResponse menu1 = OrderMenuResponse.builder()
             .menuName("간장닭강정")
@@ -35,6 +41,24 @@ public class OrderApiController {
             .orderDateTime(LocalDateTime.of(2024, 1, 17, 18, 0))
             .orderMenus(List.of(menu1, menu2))
             .build();
-        return ApiResponse.ok(response);
+        return ApiResponse.created(response);
+    }
+
+    @GetMapping("/orders")
+    public ApiResponse<SliceResponse<OrderResponse>> searchOrders(
+        @RequestParam Integer page
+    ) {
+        OrderResponse response = OrderResponse.builder()
+            .orderId(1L)
+            .storeName("나리닭강정")
+            .orderStatus("COMPLETED")
+            .totalPrice(25000)
+            .orderDateTime(LocalDateTime.of(2024, 1, 17, 18, 0))
+            .build();
+        PageRequest pageable = PageRequest.of(0, 10);
+        SliceImpl<OrderResponse> slice = new SliceImpl<>(List.of(response), pageable, false);
+
+        SliceResponse<OrderResponse> data = SliceResponse.of(slice);
+        return ApiResponse.ok(data);
     }
 }

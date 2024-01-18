117327d3fdcb9c1ab5fac26f667e14ff4d800fbd
Chaos0103 <lyt1228@naver.com>
2024-01-18 17:06:49 +0900

    feat: 회원 엔티티 생성

---
+++ server/src/main/java/com/foodwallet/server/domain/member/Member.java
@@ -0,0 +1,56 @@
+package com.foodwallet.server.domain.member;
+
+import com.foodwallet.server.domain.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Member extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    private Long id;
+
+    @Column(unique = true, nullable = false, updatable = false, length = 100)
+    private String email;
+
+    @Column(nullable = false, columnDefinition = "char(60)", length = 60)
+    private String pwd;
+
+    @Column(nullable = false, updatable = false, length = 20)
+    private String name;
+
+    @Column(nullable = false)
+    private int age;
+
+    @Column(nullable = false, updatable = false, columnDefinition = "char(1)", length = 1)
+    private String gender;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, updatable = false, columnDefinition = "varchar(10) default 'USER'", length = 10)
+    private MemberRole role;
+
+    @Embedded
+    private Account account;
+
+    @Embedded
+    private Token token;
+
+    @Builder
+    private Member(String email, String pwd, String name, int age, String gender, MemberRole role, Account account, Token token) {
+        this.email = email;
+        this.pwd = pwd;
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.role = role;
+        this.account = account;
+        this.token = token;
+    }
+}

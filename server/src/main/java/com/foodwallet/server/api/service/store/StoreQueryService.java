50b7c5597d76f9f0d48ecd1400f7c36851ab3e49
Chaos0103 <lyt1228@naver.com>
2024-01-21 01:44:29 +0900

    style: 주석 작성

--- server/src/main/java/com/foodwallet/server/api/service/store/StoreQueryService.java
+++ server/src/main/java/com/foodwallet/server/api/service/store/StoreQueryService.java
@@ -27,6 +27,14 @@ public class StoreQueryService {
     private final StoreQueryRepository storeQueryRepository;
     private final MenuQueryRepository menuQueryRepository;
 
+    /**
+     * 검색 조건을 입력 받아 매장 목록을 조회한다.
+     *
+     * @param storeType 매장 타입 조건
+     * @param query     매장명 조건
+     * @param pageable  페이징 정보
+     * @return 조회된 매장 목록 및 슬라이스 정보
+     */
     public SliceResponse<StoreResponse> searchStores(String storeType, String query, Pageable pageable) {
         StoreType type = toStoreType(storeType);
 
@@ -37,6 +45,12 @@ public class StoreQueryService {
         return SliceResponse.of(content);
     }
 
+    /**
+     * 매장 식별키와 일치하는 매장 정보와 메뉴 목록를 조회한다.
+     *
+     * @param storeId 매장의 식별키
+     * @return 조회된 매장 정보
+     */
     public StoreDetailResponse searchStore(Long storeId) {
         StoreDetailDto store = storeQueryRepository.findStoreDetailById(storeId);
 
@@ -45,6 +59,13 @@ public class StoreQueryService {
         return StoreDetailResponse.of(store, menus);
     }
 
+    /**
+     * 문자열 매장 타입을 StoreType 객체로 변환한다.
+     *
+     * @param storeType 변환할 매장 타입
+     * @return StoreType형 매장 타입
+     * @throws IllegalArgumentException 입력된 문자열 매장 타입과 일치하는 StoreType 매장 타입이 없는 경우
+     */
     private StoreType toStoreType(String storeType) {
         if (hasText(storeType)) {
             return StoreType.of(storeType);

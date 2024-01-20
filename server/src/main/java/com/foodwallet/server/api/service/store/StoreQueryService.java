680eef20601a9c5b91ec41e801a08073e45ce615
Chaos0103 <lyt1228@naver.com>
2024-01-20 15:06:52 +0900

    feat: 매장 조회 서비스 구현

--- server/src/main/java/com/foodwallet/server/api/service/store/StoreQueryService.java
+++ server/src/main/java/com/foodwallet/server/api/service/store/StoreQueryService.java
@@ -1,18 +1,38 @@
 package com.foodwallet.server.api.service.store;
 
 import com.foodwallet.server.api.SliceResponse;
+import com.foodwallet.server.domain.store.StoreType;
+import com.foodwallet.server.domain.store.repository.StoreQueryRepository;
+import com.foodwallet.server.domain.store.repository.dto.StoreSearchCond;
 import com.foodwallet.server.domain.store.repository.response.StoreResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import static org.springframework.util.StringUtils.*;
 
 @RequiredArgsConstructor
 @Service
 @Transactional(readOnly = true)
 public class StoreQueryService {
 
-    public SliceResponse<StoreResponse> searchStores(String type, String query, Pageable pageable) {
-        return null;
+    private final StoreQueryRepository storeQueryRepository;
+
+    public SliceResponse<StoreResponse> searchStores(String storeType, String query, Pageable pageable) {
+        StoreType type = null;
+        if (hasText(storeType)) {
+            type = StoreType.of(storeType);
+        }
+
+        StoreSearchCond cond = StoreSearchCond.builder()
+            .type(type)
+            .query(query)
+            .build();
+
+        Slice<StoreResponse> content = storeQueryRepository.findAllByCond(cond, pageable);
+
+        return SliceResponse.of(content);
     }
 }

c10e3d15d3c27664e6d2103237916260753bbc92
Chaos0103 <lyt1228@naver.com>
2024-01-21 18:53:18 +0900

    feat: 즐겨찾기 식별키로 즐겨찾기 조회 쿼리 구현

--- server/src/main/java/com/foodwallet/server/domain/bookmark/BookmarkId.java
+++ server/src/main/java/com/foodwallet/server/domain/bookmark/BookmarkId.java
@@ -1,12 +1,21 @@
 package com.foodwallet.server.domain.bookmark;
 
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
 @Getter
+@NoArgsConstructor
 public class BookmarkId implements Serializable {
 
     private Long member;
     private Long store;
+
+    @Builder
+    private BookmarkId(Long member, Long store) {
+        this.member = member;
+        this.store = store;
+    }
 }

28d8d4e467b3dc0623af2e13b2d3e0fe3d42dc6b
Chaos0103 <lyt1228@naver.com>
2024-01-22 01:25:10 +0900

    feat: 매장 즐겨찾기 목록 조회 서비스 구현

--- server/src/main/java/com/foodwallet/server/api/service/bookmark/BookmarkQueryService.java
+++ server/src/main/java/com/foodwallet/server/api/service/bookmark/BookmarkQueryService.java
@@ -3,8 +3,11 @@ package com.foodwallet.server.api.service.bookmark;
 import com.foodwallet.server.api.SliceResponse;
 import com.foodwallet.server.domain.bookmark.repository.BookmarkQueryRepository;
 import com.foodwallet.server.domain.bookmark.repository.response.BookmarkResponse;
+import com.foodwallet.server.domain.member.Member;
+import com.foodwallet.server.domain.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,8 +17,13 @@ import org.springframework.transaction.annotation.Transactional;
 public class BookmarkQueryService {
 
     private final BookmarkQueryRepository bookmarkQueryRepository;
+    private final MemberRepository memberRepository;
 
     public SliceResponse<BookmarkResponse> searchBookmarks(String email, Pageable pageable) {
-        return null;
+        Member member = memberRepository.findByEmail(email);
+
+        Slice<BookmarkResponse> content = bookmarkQueryRepository.findAllByMemberId(member.getId(), pageable);
+
+        return SliceResponse.of(content);
     }
 }

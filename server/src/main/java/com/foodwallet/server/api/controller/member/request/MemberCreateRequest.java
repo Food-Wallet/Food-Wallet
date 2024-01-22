5dc68906aa3f0d68e23b00a1fa4a5aad6d532455
Chaos0103 <lyt1228@naver.com>
2024-01-22 16:25:25 +0900

    feat: 신규 회원 등록 API 구현

--- server/src/main/java/com/foodwallet/server/api/controller/member/request/MemberCreateRequest.java
+++ server/src/main/java/com/foodwallet/server/api/controller/member/request/MemberCreateRequest.java
@@ -1,6 +1,9 @@
 package com.foodwallet.server.api.controller.member.request;
 
 import com.foodwallet.server.domain.member.MemberRole;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,11 +12,22 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class MemberCreateRequest {
 
+    @NotBlank(message = "이메일을 입력하세요.")
     private String email;
+
+    @NotBlank(message = "비밀번호를 입력하세요.")
     private String pwd;
+
+    @NotBlank(message = "이름을 입력하세요.")
     private String name;
+
+    @Positive(message = "출생연도를 정확하게 입력하세요.")
     private Integer birthYear;
+
+    @NotBlank(message = "성별을 입력하세요.")
     private String gender;
+
+    @NotNull(message = "회원 구분을 입력하세요.")
     private MemberRole role;
 
     @Builder

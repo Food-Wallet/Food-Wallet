ee1769cc3de4d2d4e956e7cea252af4d4c4f77c9
Chaos0103 <lyt1228@naver.com>
2024-01-24 08:46:48 +0900

    feat: 계좌 연동 인증 번호 발급 API 구현

--- server/src/main/java/com/foodwallet/server/api/controller/member/request/ConnectAccountRequest.java
+++ server/src/main/java/com/foodwallet/server/api/controller/member/request/ConnectAccountRequest.java
@@ -1,16 +1,24 @@
 package com.foodwallet.server.api.controller.member.request;
 
 import com.foodwallet.server.api.service.member.request.ConnectAccountServiceRequest;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import static com.foodwallet.server.common.message.ErrorMessage.*;
 
 @Getter
 @NoArgsConstructor
 public class ConnectAccountRequest {
 
+    @NotBlank(message = NOT_BLANK_BANK_CODE)
     private String bankCode;
+
+    @NotBlank(message = NOT_BLANK_ACCOUNT_NUMBER)
     private String accountNumber;
+
+    @NotBlank(message = NOT_BLANK_ACCOUNT_PWD)
     private String accountPwd;
 
     @Builder

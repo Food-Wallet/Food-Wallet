05825b826ff44a4bd6c6918d17f772bb09f647e6
Chaos0103 <lyt1228@naver.com>
2024-01-24 18:13:55 +0900

    test: 회원 비밀번호 수정 API 테스트 코드 구현

--- server/src/test/java/com/foodwallet/server/api/controller/member/MemberApiControllerTest.java
+++ server/src/test/java/com/foodwallet/server/api/controller/member/MemberApiControllerTest.java
@@ -3,11 +3,13 @@ package com.foodwallet.server.api.controller.member;
 import com.foodwallet.server.ControllerTestSupport;
 import com.foodwallet.server.api.controller.member.request.ConnectAccountRequest;
 import com.foodwallet.server.api.controller.member.request.MatchAuthenticationNumberRequest;
+import com.foodwallet.server.api.controller.member.request.PwdModifyRequest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -148,6 +150,72 @@ class MemberApiControllerTest extends ControllerTestSupport {
             .andExpect(jsonPath("$.code").value("400"))
             .andExpect(jsonPath("$.status").value("BAD_REQUEST"))
             .andExpect(jsonPath("$.message").value("인증 번호를 입력하세요."))
+            .andExpect(jsonPath("$.data").isEmpty());
+    }
+
+    @DisplayName("비밀번호를 수정한다.")
+    @Test
+    void modifyPwd() throws Exception {
+        //given
+        PwdModifyRequest request = PwdModifyRequest.builder()
+            .currentPwd("dong1234!")
+            .newPwd("dong8282@")
+            .build();
+
+        //when //then
+        mockMvc.perform(
+                patch(BASE_URL + "/pwd")
+                    .with(csrf())
+                    .content(objectMapper.writeValueAsString(request))
+                    .contentType(MediaType.APPLICATION_JSON)
+            )
+            .andDo(print())
+            .andExpect(status().isOk());
+    }
+
+    @DisplayName("비밀번호 수정시 현재 비밀번호는 필수값이다.")
+    @Test
+    void modifyPwdWithoutCurrentPwd() throws Exception {
+        //given
+        PwdModifyRequest request = PwdModifyRequest.builder()
+            .newPwd("dong8282@")
+            .build();
+
+        //when //then
+        mockMvc.perform(
+                patch(BASE_URL + "/pwd")
+                    .with(csrf())
+                    .content(objectMapper.writeValueAsString(request))
+                    .contentType(MediaType.APPLICATION_JSON)
+            )
+            .andDo(print())
+            .andExpect(status().isBadRequest())
+            .andExpect(jsonPath("$.code").value("400"))
+            .andExpect(jsonPath("$.status").value("BAD_REQUEST"))
+            .andExpect(jsonPath("$.message").value("현재 비밀번호를 입력하세요."))
+            .andExpect(jsonPath("$.data").isEmpty());
+    }
+
+    @DisplayName("비밀번호 수정시 새로운 비밀번호는 필수값이다.")
+    @Test
+    void modifyPwdWithoutNewPwd() throws Exception {
+        //given
+        PwdModifyRequest request = PwdModifyRequest.builder()
+            .currentPwd("dong1234!")
+            .build();
+
+        //when //then
+        mockMvc.perform(
+                patch(BASE_URL + "/pwd")
+                    .with(csrf())
+                    .content(objectMapper.writeValueAsString(request))
+                    .contentType(MediaType.APPLICATION_JSON)
+            )
+            .andDo(print())
+            .andExpect(status().isBadRequest())
+            .andExpect(jsonPath("$.code").value("400"))
+            .andExpect(jsonPath("$.status").value("BAD_REQUEST"))
+            .andExpect(jsonPath("$.message").value("새로운 비밀번호를 입력하세요."))
             .andExpect(jsonPath("$.data").isEmpty());
     }
 }

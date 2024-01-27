e82d849e449bed394eab5f4cfb960f73ce4ac1ee
Chaos0103 <lyt1228@naver.com>
2024-01-27 17:28:24 +0900

    refactor: 코드 정리

--- server/src/main/java/com/foodwallet/server/api/service/store/StoreService.java
+++ server/src/main/java/com/foodwallet/server/api/service/store/StoreService.java
@@ -13,9 +13,11 @@ import com.foodwallet.server.domain.store.repository.StoreRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 
+import static com.foodwallet.server.api.service.store.StoreValidator.*;
 import static com.foodwallet.server.common.message.ExceptionMessage.IS_NOT_BUSINESS_MEMBER;
 import static com.foodwallet.server.common.message.ExceptionMessage.NO_ACCOUNT_INFORMATION;
 
@@ -40,19 +42,22 @@ public class StoreService {
         }
 
         StoreType type = StoreType.of(request.getType());
-        String validatedName = StoreValidator.nameValidation(request.getName());
-        String validatedDescription = StoreValidator.descriptionValidation(request.getDescription());
+        String validatedName = nameValidation(request.getName());
+        String validatedDescription = descriptionValidation(request.getDescription());
 
-        UploadFile uploadFile;
-        try {
-            uploadFile = fileStore.storeFile(request.getImage());
-        } catch (IOException e) {
-            throw new IllegalArgumentException("파일 업로드를 실패했습니다.", e);
-        }
+        UploadFile uploadFile = toUploadFile(request.getImage());
 
         Store store = Store.createStore(type, validatedName, validatedDescription, uploadFile, member);
         Store savedStore = storeRepository.save(store);
 
         return StoreCreateResponse.of(savedStore);
+    }
+
+    private UploadFile toUploadFile(MultipartFile file) {
+        try {
+            return fileStore.storeFile(file);
+        } catch (IOException e) {
+            throw new IllegalArgumentException("파일 업로드를 실패했습니다.", e);
+        }
     }
 }
